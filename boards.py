5857a485031830a61eed9311ee5f94e8e75ccc4d
Simply-Aurtistic <98486001+Simply-Aurtistic@users.noreply.github.com>
2022-02-02 21:04:54 -0500

    Update boards.py

--- boards.py
+++ boards.py
@@ -3,13 +3,16 @@
 class Level:
     def __init__(self):
         self.difficulty = 0
+        self.set_level()
 
-    def setlevel(self):
-        self.difficulty = int(input("What difficulty would you like? 1 - 10(1 = EASY 10 = HARD)"))
+    def set_level(self):
+        self.difficulty = int(input("What difficulty would you like? 1 - 10(1 = EASY 10 = HARD): "))
+
+    def get_level(self):
+        return self.difficulty
 
 
 class Forest(Level):
 
     def __init__(self):
         super().__init__()
-        self.setlevel()
