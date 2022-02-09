c93d610a5161519291b69a9ad293efbe33813e5e
Simply-Aurtistic <98486001+Simply-Aurtistic@users.noreply.github.com>
2022-02-08 21:27:52 -0500

    Update shop.py

--- shop.py
+++ shop.py
@@ -15,8 +15,10 @@ class Shop:
         self.sworddamage = self.load_data.iat[6, 2]
         self.bowdamage = self.load_data.iat[7, 2]
         self.defense = self.load_data.iat[8, 2]
-        self.shopping = True
+        self.low_potion = self.load_data.iat[9, 2]
+        self.high_potion = self.load_data.iat[10, 2]
         self.hp = self.max_health
+        self.shopping = True
 
     def store(self):
         while self.shopping:
@@ -25,8 +27,9 @@ class Shop:
                   f"Would you like to upgrade your armor for {self.armor_upgrade_costs} gold, \n"
                   f"your sword damage for {self.sword_upgrade_costs} gold, \n"
                   f"your bow damage for {self.bow_upgrade_costs} gold, \n"
-                  f"or your health for {self.health_upgrade_costs} gold?")
-            shop_choice = input("(armor/sword/bow/health/back): ").lower()
+                  f"your health for {self.health_upgrade_costs} gold?\n"
+                  f"or would you like a buy a low potion for 5 gold or high potion for 20 gold")
+            shop_choice = input("(armor/sword/bow/health/low/high/back): ").lower()
 
             if shop_choice == "armor":
                 if int(self.gold) >= int(self.armor_upgrade_costs):
@@ -58,6 +61,17 @@ class Shop:
                 else:
                     print("Not enough money")
 
+            elif shop_choice == "low":
+                if self.gold >= 5:
+                    self.low_potion += 1
+                else:
+                    print("Not enough money")
+            elif shop_choice == "high":
+                if self.gold >= 20:
+                    self.high_potion += 1
+                else:
+                    print("Not enough money")
+
             elif shop_choice == "back":
                 self.shopping = False
                 return
@@ -85,10 +99,26 @@ class Shop:
                   " \n"
                   "'Hello my name is Beelzebub, but you can call me Buzz. You must be that new adventurer I have heard "
                   "about' \n"
-                  "Buzz turns and gets back behind the counter. 'What can i get for you today.'")
-            self.store()
-            
+                  "Buzz turns and gets back behind the counter. 'Here. Take this bow, it will help you with the fights "
+                  "to come.' \n"
+                  "As you turn to leave Buzz calls out, 'Good luck and come back if you need anything, you hear'")
+
         else:
-            game_load = input("What is the save name: ") + ".csv"
-            self.load_data = pandas.read_csv(game_load)
+            self.game_load = input("What is the save name: ").lower()
+            self.game_load_save = self.game_load + ".csv"
+            self.load_data = pandas.read_csv(self.game_load_save)
             return self.load_data
+
+    def damage1(self, enemy):
+        defense = round(self.sworddamage / 2) + self.defense
+        damage = enemy - defense
+        if damage < 0:
+            damage = 0
+        self.hp -= damage
+
+    def damage2(self, enemy):
+        defense = self.defense
+        damage = enemy - defense
+        if damage < 0:
+            damage = 0
+        self.hp -= damage
